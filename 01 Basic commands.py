2c28410db666c7b25e8f077f32668f03b87fdbbb
TEJA <g.yuvateja1870@gmail.com>
2026-02-25 19:12:56 +0530

    Add files via upload

---
+++ 01 Basic commands.py
@@ -0,0 +1,91 @@
+# print()
+
+print("hi, this is my first python code")
+print("hello python")
+
+print("-------------------------")
+print("    python learning      ")
+print("-------------------------")
+
+
+# Special characters 
+
+print("hi \"python\"")    # \" Double Quote
+
+print('hi \'python\'')    # \' Single Quote
+
+print("hi : teja\\sai")   # \\ Backslash
+
+print("Teja\nSai")        # \n New Line
+
+print("Yuva\tTeja")       # \t Tab
+
+print("Yuva\bTeja")       # \b Backspace
+
+# print
+# Multi special Character task
+
+print("Your learning path :\n\t- python basics\n\t- Data Engineering\n\t- AI")
+
+print("""Your learning path :
+\t- python basics
+\t- Data Engineering
+\t- AI""")        #  Multi Double Quotes 
+
+
+#  Variables
+
+print("My name is Teja")
+print("Teja is learning python")
+print("Teja want to become python expert")
+
+
+name = "Teja"
+print("My name is", name)
+print(name, "is learning python")
+print(name, "want to become python expert")
+
+
+name = "Sai"
+print("My name is",name)
+print(name,"is learning python")
+print(name,"want to become python expert")
+
+
+name = "Teja"
+language = "python"
+print("My name is", name)
+print(name, "is learning", language)
+print(name, "want to become", language, "expert")
+
+# Variable 
+# Task
+
+name = "datawithteja.com"
+print("info@"+ name)
+print("support@"+ name)
+print("www."+ name)
+
+
+
+
+# input()
+
+name = input("Enter your name:")
+print("You are", name)
+
+name = input("Enter your name:")
+country = "India"
+print(name, "comes from", country)
+
+name = input("Enter your name:")
+country = input("Enter your country:")
+print(name, "comes from", country)
+
+
+# Task for print(), variable and input()
+
+x=1
+print(x)
+y= input("Enter value:")
+print(y)
